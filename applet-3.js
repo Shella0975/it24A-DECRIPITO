d0fc329108df8fc89a88c894003e8a002ebf7cbd
Shella0975 <decrepitoshellamae@gmail.com>
2024-10-11 15:17:45 +0800

    Added

--- applet-3.js
+++ applet-3.js
@@ -33,8 +33,15 @@ class DataLogger {
             this.idContainer.appendChild(card);
         });
 
-      
-  
+        this.displayLogCount();
+    }
+    countLogs() {
+        return this.loggedData.length;
+    }
+    displayLogCount() {
+        const logCount = this.countLogs();
+        this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`;
+    }
 }
 
 
