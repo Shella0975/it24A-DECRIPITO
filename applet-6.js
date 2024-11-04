f2bd1a291ad7752f0b4c93e5fa809cca87d5f6ca
Shella0975 <decrepitoshellamae@gmail.com>
2024-11-04 12:30:34 +0800

    added edit task method in applet-6.js

--- applet-6.js
+++ applet-6.js
@@ -53,3 +53,9 @@ class todoList{
     removeTask(event) {
         this.todoList.removeChild(event.target.closest('.todo-item'));
     }
+    editTask(event) {
+        const taskItem = event.target.closest('.todo-item');
+        this.todoInput.value = taskItem.querySelector('.task-text').textContent;
+        this.editingIndex = Array.from(this.todoList.children).indexOf(taskItem);
+        this.addButton.textContent = 'Update';
+    }
